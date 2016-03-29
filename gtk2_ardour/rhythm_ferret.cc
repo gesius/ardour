384344d29236b0dcf6a2c13c8effa8e9aa6906b3
nick_m <mainsbridge@gmail.com>
2016-03-29 16:48:56 +1100

    Perfornamce - remove dubious optimisation.

--- gtk2_ardour/rhythm_ferret.cc
+++ gtk2_ardour/rhythm_ferret.cc
@@ -350,8 +350,6 @@ RhythmFerret::do_split_action ()
 		return;
 	}
 
-	editor.EditorFreeze(); /* Emit signal */
-
 	editor.begin_reversible_command (_("split regions (rhythm ferret)"));
 
 	/* Merge the transient positions for regions in consideration */
@@ -382,8 +380,6 @@ RhythmFerret::do_split_action ()
 	}
 
 	editor.commit_reversible_command ();
-
-	editor.EditorThaw(); /* Emit signal */
 }
 
 void

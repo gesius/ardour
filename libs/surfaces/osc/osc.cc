faa9c17fac5b97b25065580965d040c8312bfbd4
Paul Davis <paul@linuxaudiosystems.com>
2016-04-08 16:49:47 -0400

    universal change in the design of the way Route/Track controls are designed and used. The controls now own their own state, rather than proxy for state in their owners.
    Massive changes all over the code to accomodate this. Many things are not finished. Consider this a backup safety commit

--- libs/surfaces/osc/osc.cc
+++ libs/surfaces/osc/osc.cc
@@ -975,7 +975,7 @@ OSC::routes_list (lo_message msg)
 					|| boost::dynamic_pointer_cast<MidiTrack>(r)) {
 
 				boost::shared_ptr<Track> t = boost::dynamic_pointer_cast<Track>(r);
-				lo_message_add_int32 (reply, t->record_enabled());
+				lo_message_add_int32 (reply, (int32_t) t->rec_enable_control()->get_value());
 			}
 
 			//Automatically listen to routes listed
@@ -1055,7 +1055,7 @@ OSC::route_mute (int rid, int yn)
 	boost::shared_ptr<Route> r = session->route_by_remote_id (rid);
 
 	if (r) {
-		r->set_mute (yn, PBD::Controllable::NoGroup);
+		r->mute_control()->set_value (yn ? 1.0 : 0.0, PBD::Controllable::NoGroup);
 	}
 
 	return 0;
@@ -1069,9 +1069,7 @@ OSC::route_solo (int rid, int yn)
 	boost::shared_ptr<Route> r = session->route_by_remote_id (rid);
 
 	if (r) {
-		boost::shared_ptr<RouteList> rl (new RouteList);
-		rl->push_back (r);
-		session->set_solo (rl, yn, Session::rt_cleanup, PBD::Controllable::NoGroup);
+		r->solo_control()->set_value (yn ? 1.0 : 0.0, PBD::Controllable::NoGroup);
 	}
 
 	return 0;
@@ -1085,7 +1083,10 @@ OSC::route_recenable (int rid, int yn)
 	boost::shared_ptr<Route> r = session->route_by_remote_id (rid);
 
 	if (r) {
-		r->set_record_enabled (yn, PBD::Controllable::NoGroup);
+		boost::shared_ptr<Track> trk = boost::dynamic_pointer_cast<Track> (r);
+		if (trk) {
+			trk->rec_enable_control()->set_value (yn, PBD::Controllable::UseGroup);
+		}
 	}
 
 	return 0;
@@ -1099,7 +1100,7 @@ OSC::route_set_gain_abs (int rid, float level)
 	boost::shared_ptr<Route> r = session->route_by_remote_id (rid);
 
 	if (r) {
-		r->set_gain (level, PBD::Controllable::NoGroup);
+		r->gain_control()->set_value (level, PBD::Controllable::NoGroup);
 	}
 
 	return 0;

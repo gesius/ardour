9758390e79bba81e0e9f99db7a5b92cef9f73aa3
nick_m <mainsbridge@gmail.com>
2016-04-14 06:03:20 +1000

    Tempo ramps - adding a new tempo doesn't involve a dialog.

--- libs/ardour/tempo.cc
+++ libs/ardour/tempo.cc
@@ -1484,6 +1484,13 @@ TempoMap::pulse_at_frame_locked (const Metrics& metrics, const framecnt_t& frame
 	return pulses_in_section + prev_t->pulse();
 }
 
+double
+TempoMap::pulse_at_frame (const framecnt_t& frame) const
+{
+	Glib::Threads::RWLock::ReaderLock lm (lock);
+	return pulse_at_frame_locked (_metrics, frame);
+}
+
 framecnt_t
 TempoMap::frame_at_pulse_locked (const Metrics& metrics, const double& pulse) const
 {
@@ -1512,6 +1519,13 @@ TempoMap::frame_at_pulse_locked (const Metrics& metrics, const double& pulse) co
 	framecnt_t const ret = (framecnt_t) floor (dtime) + prev_t->frame();
 
 	return ret;
+}
+
+framecnt_t
+TempoMap::frame_at_pulse (const double& pulse) const
+{
+	Glib::Threads::RWLock::ReaderLock lm (lock);
+	return frame_at_pulse_locked (_metrics, pulse);
 }
 
 double
